36d88873e07c2600949486b96a24bca0169daf0c
heixhei <2724391331@qq.com>
2023-07-03 20:25:00 +0800

    增加了公共模块common，优化了依赖管理，增加了公共配置文件，需要放到resource/config下

---
+++ common/src/main/java/com/baiyuechu/App.java
@@ -0,0 +1,13 @@
+package com.baiyuechu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
